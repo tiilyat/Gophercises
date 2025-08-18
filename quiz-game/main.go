86e2012cbc046c41b8362140cc953a52cac6b4f0
Тетерич Илья Ильхамович <i.teterich@tinkoff.ru>
2025-08-18 18:33:56 +0300

    Инициализация

---
+++ quiz-game/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Parse flag with csv file name, if empty read default 'problems.csv'
+
+	// Read csv file
+	// Save question in map [question]answer
+
+	// initialize map with stats (questions size, right answers, wrong answers)
+
+	// Loop questions
+	// Ask N question. If right or wrong save in stats
+
+	// Print results like "You scored N out of len(questions)"
+	fmt.Println("Hello World")
+}
