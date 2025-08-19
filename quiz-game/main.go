37b717e359971133d063a6d68d088068d8d952ba
Тетерич Илья Ильхамович <i.teterich@tinkoff.ru>
2025-08-19 17:33:01 +0300

    wip

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -1,8 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
+
+var csvPath string
+
+func init() {
+
+	flag.StringVar(
+		&csvPath,
+		"csv",
+		"problems.csv",
+		"a csv file in the format of 'question,answer' (default \"problems.csv\")",
+	)
+}
 
 func main() {
+
+	file, err := os.Open(csvPath)
+
+	if err != nil {
+		fmt.Printf("Ошибка: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.Comma = ','
+	reader.TrimLeadingSpace = true
+
+	records, err := reader.ReadAll()
+	if err != nil {
+		fmt.Printf("Ошибка чтения: %v\n", err)
+		return
+	}
+
+	for _, record := range records {
+		fmt.Printf("Запись: %v\n", record)
+	}
 	// Parse flag with csv file name, if empty read default 'problems.csv'
 
 	// Read csv file
@@ -14,5 +53,4 @@ func main() {
 	// Ask N question. If right or wrong save in stats
 
 	// Print results like "You scored N out of len(questions)"
-	fmt.Println("Hello World")
 }
