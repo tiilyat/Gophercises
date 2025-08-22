c6842d7e17d329c5df2ae79a86584578bdef5f62
tiilyat <teterich.ilya@gmail.com>
2025-08-22 15:00:37 +0300

    mend

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -5,9 +5,12 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
+	"time"
 )
 
 var csvPath string
+var timeLimit int
 
 func init() {
 
@@ -17,6 +20,10 @@ func init() {
 		"problems.csv",
 		"a csv file in the format of 'question,answer' (default \"problems.csv\")",
 	)
+
+	flag.IntVar(&timeLimit, "limit", 30, "the time limit for the quiz in seconds (default 30)")
+
+	flag.Parse()
 }
 
 func main() {
@@ -24,33 +31,86 @@ func main() {
 	file, err := os.Open(csvPath)
 
 	if err != nil {
-		fmt.Printf("Ошибка: %v\n", err)
-		return
+		exit(fmt.Sprintf("Failed to open the CSV file: %v\n", err))
 	}
 	defer file.Close()
 
 	reader := csv.NewReader(file)
-	reader.Comma = ','
-	reader.TrimLeadingSpace = true
 
 	records, err := reader.ReadAll()
 	if err != nil {
-		fmt.Printf("Ошибка чтения: %v\n", err)
-		return
+		exit(fmt.Sprintf("Failed to read the CSV file: %v\n", err))
 	}
 
-	for _, record := range records {
-		fmt.Printf("Запись: %v\n", record)
+	problems := parseLines(records)
+
+	questionsSize := len(records)
+	correct := 0
+
+	fmt.Println("Press enter to start the quiz")
+	fmt.Scanln()
+	fmt.Println("Starting the quiz...")
+
+problemLoop:
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.question)
+		answerCh := make(chan string, 1)
+		timer := time.NewTimer(time.Duration(timeLimit) * time.Second)
+
+		go func() {
+
+			var userAnswer string
+			_, err := fmt.Fscan(os.Stdin, &userAnswer)
+
+			if err != nil {
+				exit(fmt.Sprintf("Failed to read user answer: %v\n", err))
+			}
+
+			select {
+			case answerCh <- userAnswer:
+			default:
+			}
+
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println("Time's up!")
+			break problemLoop
+		case answer := <-answerCh:
+			timer.Stop()
+			if formatAnswer(answer) == formatAnswer(p.answer) {
+				correct++
+			}
+		}
 	}
-	// Parse flag with csv file name, if empty read default 'problems.csv'
 
-	// Read csv file
-	// Save question in map [question]answer
+	fmt.Printf("You scored %d out of %d.\n", correct, questionsSize)
+}
 
-	// initialize map with stats (questions size, right answers, wrong answers)
+func formatAnswer(answer string) string {
+	return strings.TrimSpace(strings.ToLower(answer))
+}
 
-	// Loop questions
-	// Ask N question. If right or wrong save in stats
+func parseLines(lines [][]string) []problem {
+	problems := make([]problem, len(lines))
 
-	// Print results like "You scored N out of len(questions)"
+	for i, line := range lines {
+		problems[i] = problem{
+			question: line[0],
+			answer:   line[1],
+		}
+	}
+
+	return problems
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
+
+type problem struct {
+	question string
+	answer   string
 }
